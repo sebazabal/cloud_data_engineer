46c787f2c56065dcc4bc23cc3c542daf9c040881
sebazabal <zabaljaureguisebastian@gmail.com>
2022-05-25 16:26:57 -0300

    changes in files

--- add_data/add_data.py
+++ add_data/add_data.py
@@ -1,11 +1,21 @@
-from distutils.log import error
-from matplotlib.pyplot import table
 import pandas as pd 
 from sqlalchemy import create_engine
 import logging 
+import traceback
 
-
-
+logger = logging.getLogger('Log_add_data')
+logger.setLevel(logging.DEBUG)
+fh = logging.FileHandler('debug_add_data.log')
+fh.setLevel(logging.DEBUG)
+logger.addHandler(fh)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+logger.addHandler(fh)
+#logger.debug('mensaje debug')
+#logger.info('mensaje info')
+#logger.warning('mensaje warning')
+#logger.error('mensaje error')
+#logger.critical('mensaje critical')
 host_ = "ps_12_7"
 database_ = "crypto_db"
 user_ = "postgres"
@@ -13,19 +23,38 @@ pass_ = "postgres"
 port_ = "5432"
 
 # host, database, user, pass, port
-engine = create_engine(f'postgresql://{user_}:{pass_}@{host_}:{port_}/{database_}')
+#logger.info('mensaje info')
+logger.info("create engine")
+try:
+    engine = create_engine(f'postgresql://{user_}:{pass_}@{host_}:{port_}/{database_}')
+except Exception as e:
+    logger.error("create engine failed")
+    logger.error(f"Error message: {e}")
+
 
 #engine.connect()
 
 # Define data path to download datasets
 #df = pd.read_csv("nft_data.csv")
-df = pd.read_csv("data_file_nft.csv")
-
+logger.info("read csv file")
+try:
+    df = pd.read_csv("data_file_nft.csv")
+except Exception as er:
+    logger.error("error while reading file")
+    logger.error(f"error message: {e}")
+    
 # Use SQLAlchemy to upload DataFrame to database in postgreSQL
 #Base, conection, if exists, index
-df.to_sql(table = "nft_data",con = engine, if_exists='replace',index=True, schema= "public")
+logger.info("df to SQL")
+try:
+    df.to_sql(name = "nft_data",con = engine, if_exists='replace',index=True, schema= "public")
+except Exception as e:
+    logger.error("Error while df_toSQL")
+    logger.error(f"Error message: {e}")
+    #logging.error(traceback.format_exc())
+    #print(traceback.print_exc())
+    # Logs the error appropriately. 
 
-print("df.to_SQL corrió")
 #
 # Close Connection
 engine.dispose()
